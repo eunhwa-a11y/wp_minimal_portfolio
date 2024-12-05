ed8160a3ddd1f7c0f235207d3f42d67ecf9169fe
eunhwa-a11y <ttab136@gmail.com>
2024-12-05 14:00:33 +0900

    페이지네이션 마무리 및 contact.php 생성

--- category.php
+++ category.php
@@ -1,46 +1,49 @@
 <?php get_header('portfolio') ?>
 
   <div class="container latest_portfolio">            
-      <div class="row list">
+    <div class="row list">
 
-      <?php      
+    <?php      
 
-        // The Loop
-        while ( have_posts() ) : the_post();
+      // The Loop
+      while ( have_posts() ) : the_post();
+
+    ?>
+
+    <div class="col-md-4">
+      <div class="contents shadow">
+        <?php
+          if ( has_post_thumbnail() ) {
+              the_post_thumbnail( 'full' );
+          }
         ?>
-
-        <div class="col-md-4">
-            <div class="contents shadow">
-                <?php
-                  if ( has_post_thumbnail() ) {
-                      the_post_thumbnail( 'full' );
-                  }
-                ?>
-                <div class="hover_contents">
-                    <div class="list_info">
-                        <h3>
-                          <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> 
-                          <img src="<?= IMAGES ?>/portfolio_list_arrow.png" alt="list arrow">
-                        </h3>
-                        <p><a href="<?php the_permalink(); ?>">Click to see project</a></p>
-                    </div>
-                </div>
-            </div>
+        <div class="hover_contents">
+          <div class="list_info">
+            <h3>
+            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> 
+            <img src="<?= IMAGES ?>/portfolio_list_arrow.png" alt="list arrow">
+            </h3>
+            <p><a href="<?php the_permalink(); ?>">Click to see project</a></p>
+          </div>
         </div>
-
-      <?php 
-        endwhile;
-        // Reset Query
-        wp_reset_query();    
-      ?>
-              
       </div>
-      <p class="pagenation shadow">
-          <a href="" class="secondary-btn active">1</a>      
-          <a href="" class="secondary-btn">2</a>      
-          <a href="" class="secondary-btn">3</a>      
-          <a href="" class="secondary-btn">4</a>      
-      </p>
-  </div>
+    </div>
+
+    <?php 
+      endwhile;
+      // Reset Query
+      wp_reset_query();    
+    ?>
+            
+    </div>
+     <!-- <p class="pagenation shadow"> -->
+        <!-- <a href="" class="secondary-btn active">1</a>      
+        <a href="" class="secondary-btn">2</a>      
+        <a href="" class="secondary-btn">3</a>      
+        <a href="" class="secondary-btn">4</a>       -->
+
+        <?php minimal_pagination() ?>
+      <!-- </p> -->
+    </div>
   
 <?php get_footer() ?>
